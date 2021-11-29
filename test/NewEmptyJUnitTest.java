20d8e9929d4d7b5425d2dbad71371483a874d715
daesparza2 <daesparza2@espe.edu.ec>
2021-11-29 11:56:03 -0500

    ejercicio acme ioet pruebas

--- test/NewEmptyJUnitTest.java
+++ test/NewEmptyJUnitTest.java
@@ -23,6 +23,6 @@ public class NewEmptyJUnitTest {
         String cadena2 = "MO10:00-12:00,TH12:00-14:00,SU20:00-21:00";
         EmpleadoControlador controlador = new EmpleadoControlador();
         int coincide = controlador.compararHorario(cadena1.split(","), cadena2.split(","));
-        assertEquals(3, coincide);
+        assertEquals(2, coincide);
     }
 }
